bd19330b7fcce8d22a5f94c5e03c988b8e3da5c9
Vijay Nannapuraju <59799317+vijayn7@users.noreply.github.com>
2024-01-02 15:20:24 -0500

    added Courier & Helvetica font

--- TextEditor.py
+++ TextEditor.py
@@ -14,5 +14,25 @@ def saveas():
     file1.close()
 
 button=Button(root, text="Save", command=saveas)
-button.grid() 
+button.grid()
+
+def FontHelvetica():
+    global text
+    text.config(font="Helvetica")
+
+def FontCourier():
+    global text
+    text.config(font="Courier")
+
+font=Menubutton(root, text="Font") 
+font.grid() 
+font.menu=Menu(font, tearoff=0) 
+font["menu"]=font.menu
+helvetica=IntVar() 
+courier=IntVar()
+font.menu.add_checkbutton(label="Courier", variable=courier,
+command=FontCourier)
+font.menu.add_checkbutton(label="Helvetica", variable=helvetica, 
+command=FontHelvetica)
+
 root.mainloop()
